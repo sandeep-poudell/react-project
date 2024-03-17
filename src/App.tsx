31c32f55940f4d14468e417dbeda1c7eb51f4469
Sandeep Poudel <poudel.sandeep@cloco.co.jp>
2024-03-17 23:41:04 +0900

    passing children props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import ListGroup from './components/ListGroup.tsx';
+import Alert from "./components/Alert.tsx";
 
 function App() {
-    let items = ["Tokyo", "Kyoto", "Hokkaido", "Osaka", "Fukuoka"]
-    const handleSelectItem = (item: string) => {
-        console.log(item)
-    }
-    return <div><ListGroup items={items} heading="Cities" onSelected={handleSelectItem}/></div>;
+    return <div>
+        <Alert>
+            Hello World
+            <h5>Happy Programming!</h5>
+        </Alert>
+    </div>;
 }
 
 export default App;
---
+++ src/components/Alert.tsx
@@ -0,0 +1,15 @@
+import {ReactNode} from "react";
+
+interface AlertProps {
+    children: ReactNode
+}
+
+const Alert = ({children}: AlertProps) => {
+    return (
+        <div className={'alert alert-primary'}>
+            {children}
+        </div>
+    );
+};
+
+export default Alert;
